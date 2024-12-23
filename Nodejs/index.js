6b9b709beb1fca45f68bed38d56145a1c98b2dca
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-24 02:17:24 +0500

    code is added

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,3 +1,11 @@
 const a = 20
 
 console.log(a/10);
+
+// Ab ye backend klye kam ni kryga ku k backend m document.getElementById ya iss trhan ki chezain ni hoti 
+// ye specifically front end klye hoti hai browser pr work krygi but node js pr jb m run krogi terminal pr tu gives me error
+// ku k windows object specifically front end klye hai na k backend klye...
+
+
+// But index.js wali file or fron end m jo index.js file hoti hai wo same e hoti hai...
+console.log(window);
