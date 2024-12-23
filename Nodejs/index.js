33c3db2c81c9d41651e09d046c94bbdc62594a52
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-24 03:37:47 +0500

    code is added

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -127,3 +127,6 @@ console.log(extension_name);        // .js ==>File ki extension bata daiga..
 console.log(basename);              // Nodejs ==> Currently jis folder m hai oss folder ka nam bata daiga...
 console.log(directory_name);         // D:/Backened(Node JS, Express, Mongo Db)/Nodejs/ ==> Pura path bata daiga and jis directory k andr hai..
 console.log(path_join);             // Jo path meny dea hai osk sath "/6pp" ko b add krdyga...
+
+
+//===========================Some More Fun with os===================================//
