a0aca5376c1fe00a21c36d864cbe379adbdf44c7
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-24 02:33:30 +0500

    code is added

---
+++ Nodejs/app.js
@@ -0,0 +1,5 @@
+// Ab meny index.js k andr jo export kea hai osko m yahan import krogi...
+
+const b = require("./index.js");
+
+console.log(b);
--- Nodejs/index.js
+++ Nodejs/index.js
@@ -5,13 +5,39 @@ console.log(a/10);
 // ye specifically front end klye hoti hai browser pr work krygi but node js pr jb m run krogi terminal pr tu gives me error
 // ku k windows object specifically front end klye hai na k backend klye...
 // But index.js wali file or fron end m jo index.js file hoti hai wo same e hoti hai...
-console.log(window);
+// console.log(window);
 
 
-// Learn Node js Now:
+// ==============Learn Node js Now:==========================
 
 // Node js k andr me function banao, ya object banano, ya variable banao hr chez module hai node js k andr...
 
 // Ab Modules different types k hoty hain:
 
 // i---File based module(Transfer from one file to another)
+
+// File based module(Example 01)
+
+const b = 8;
+const c = 80;
+console.log(c);
+
+// Example 02:
+
+const d = {
+    average: (a, b) => {
+        return ((a + b) / 2);
+    },
+    multiply: (a, b) => {
+        return (a * b);
+    },
+    squareRoot: (a) => {
+        return Math.sqrt(a);
+    },
+    percent: (a,b) => {
+        return ((a / b) * 100);
+    }
+}
+
+module.exports = b;
+
