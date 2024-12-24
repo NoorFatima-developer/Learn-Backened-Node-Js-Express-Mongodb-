3857d6f4d99bb41e995e36800139d4e1688ea0ad
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-24 21:10:34 +0500

    code is added

--- Express/index.js
+++ Express/index.js
@@ -13,37 +13,37 @@ const Path = require("path");
 
 // 02--call express:
  const app = express();
- const port = 3000;
+ const port = 5000;
  
 
 // 04-- Get Express:(GET METHOD)
-app.get("/", r=(req, res)=>{
+app.get("/", (req, res)=>{
 // res.send("HOME PAGE");
 // Mer ps index.html file hai and meny wo file bejni hai home page ki bjeye tu i will use this;
-    res.sendFile(Path.join(__dirname, "index.html"));
+    res.sendFile(Path.join(__dirname, "/index.html"));
+ });
 
-//  });
+
 // app.get("/about", r=(req, res)=>{
 //     res.send("ABOUT PAGE");
 //  });
 // app.get("/contact", r=(req, res)=>{
 //     res.send("CONTACT PAGE");
+// });
+
 
 // Abi ye sary crud operations GET, POST, PUT, DELETE hum log sirf home pr apply krygy...
 
 //POST METHOD USE:(Jesy e index.html wali file send kryga ye osmai jo b data hum dalygy wo url mai show hojyega...)
-app.post("/", (req, res)=> {
+app.post("/api/v1/login", (req, res)=> {
     // ab let say meny 1 form bra hai osmai meny noor pass kea hai ab mai nichy wlai line sy noor ko access krskti hon...
 
-    const username = req.body.name
-    console.log(username);
-
-
+    // const username = req.body.name
+    // console.log(username);
+    res.send("<h1>DONE</h1>")
+    console.log("name");
     
 })
-
-});
-
 
 
 // 03--Listen:
