de8a51b1e35ee88db64ac292d96127e300daea27
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-09 04:26:30 +0500

    code is added

--- Nodejs/createserver.js
+++ Nodejs/createserver.js
@@ -1,7 +1,20 @@
 // 01---Create Server...
 const http = require("http");
-const server = http.createServer( ()=> {
-    console.log("Server started on port 3000");
+// req , res k bagair ye local host pr ni chlyga...
+const server = http.createServer( (req, res)=> {
+    // res.end("<h1>Create Server</h1>");
+    if(req.url === "/"){
+        res.end("<h1>Home Page</h1>");
+    }
+    if(req.url === "/about"){
+        res.end("<h1>About Page</h1>");
+    }
+    if(req.url === "/contact"){
+        res.end("<h1>Contact Page</h1>");
+    }
+    else {
+        res.end("<h1>Page Not Found....</h1>");
+    }
 })
 
 // 02---Listen for requests...
