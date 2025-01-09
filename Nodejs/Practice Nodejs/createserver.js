daee25c9685aece2c2ad8e648e992413ea467cd2
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-09 17:31:16 +0500

    code is added

---
+++ Nodejs/Practice Nodejs/createserver.js
@@ -0,0 +1,41 @@
+// const http = require("http");
+// import filebased module and use it...
+// Old way to import...
+// const gfName = require("./feature.js");
+// New way to import...
+
+import http from "http";
+import gfName from "./feature.js";
+console.log(gfName);
+
+
+
+// 01---Create Server...
+// req , res k bagair ye local host pr ni chlyga...
+const server = http.createServer( (req, res)=> {
+    // res.end("<h1>Create Server</h1>");
+    if(req.url === "/"){
+        res.end("<h1>Home Page</h1>");
+    }
+    else if(req.url === "/about"){
+        res.end("<h1>About Page</h1>");
+    }
+    else if(req.url === "/contact"){
+        res.end("<h1>Contact Page</h1>");
+    }
+    else {
+        res.end("<h1>Page Not Found....</h1>");
+    }
+})
+
+// 02---Listen for requests...
+server.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
+
+
+
+// Note:
+
+// Everything in nodejs is module... and there are three types of modules in nodejs..and hr 1 ko meny
+// briefly explain nd solve kea hai index.js k andr...
---
+++ Nodejs/Practice Nodejs/feature.js
@@ -0,0 +1,10 @@
+// Old way hai import and export ka...
+
+// const gfName =  "MsNoor";
+// module.exports = gfName;
+
+// New way hai import and export ka...
+
+const gfName =  "MsNoor";
+
+export default gfName;
