492a5e13c11eeea08e1b3dedeaa5a8d091f4a487
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-10 19:37:23 +0500

    code is added

--- Express/Practice Express/index1.js
+++ Express/Practice Express/index1.js
@@ -1,15 +1,14 @@
 import express from 'express';
 
 // 01-- create server...
-
 const app = express();
 
 // 03--- Ab data send krny klye hmy if else conditions ni lagani prygi jesy hum node js mai lagaty thy wo hum yahan pr bach jygy on conditions sy...
 // USE GET method to send data...
-
 app.get('/getproducts', (req, res) => {
     // res.send("Hi");
     // res.sendStatus(404);
+    // res.status(200).send("Meri mrzi")
     // And ismai hum data json format m b zada tr send krty hain...
     res.json({
        success : true,
@@ -18,9 +17,7 @@ app.get('/getproducts', (req, res) => {
 })
 
 // 02-- listen server...
-
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
 
-// 03-- define routes...
