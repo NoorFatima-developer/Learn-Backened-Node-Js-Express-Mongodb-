1504ab918d75595da243362e5c3ee98cb75fb476
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-13 04:38:38 +0500

    code is added

--- Mongo_db/index.js
+++ Mongo_db/index.js
@@ -1,14 +1,20 @@
 // Old way to import..
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 // New way to import..
-import mongoose from 'mongoose';
+import mongose from 'mongoose';
 
 
 //Now connect mongodb to Nodejs using mongoose...
 
 // .then sy it will return a promise...
-mongoose.connect(' mongodb://127.0.0.1:27017').then(() => {
+// mongose.connect('mongodb://127.0.0.1:27017', {useNewURLParser: true, useUnifiedTopology: true}).then(() => {
+//     console.log("connected to MongoDB successfully");   
+// }).catch(err => console.log(err));
+
+mongose.connect('mongodb://127.0.0.1:27017').then(() => {
     console.log("connected to MongoDB successfully");   
 }).catch(err => console.log(err));
 
 
+// Ab isk andr humy model or schema pehly sy khud e banana hoga ta k hum crud operations 
+// perform krskain ku k isk andr crud operations different hoty trhan sy hoty hain..shell or compass ki trhan ni...
