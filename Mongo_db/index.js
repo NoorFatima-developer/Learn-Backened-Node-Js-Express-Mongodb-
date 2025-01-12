7be4d4434e4c410f27fc1383255d946e49efe24b
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-13 04:25:48 +0500

    code is added

--- Mongo_db/index.js
+++ Mongo_db/index.js
@@ -7,7 +7,8 @@ import mongoose from 'mongoose';
 //Now connect mongodb to Nodejs using mongoose...
 
 // .then sy it will return a promise...
-mongoose.connect('mongodb://localhost:').then(() => {
+mongoose.connect(' mongodb://127.0.0.1:27017').then(() => {
     console.log("connected to MongoDB successfully");   
 }) 
+
 
