cb71862ddabd1f32e445868224825388debecf72
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-18 17:47:15 +0500

    code is added

---
+++ NodeAPI/app.js
@@ -0,0 +1,17 @@
+//01--- import express:
+import express from 'express';
+
+//02--- create server:
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');  // when we go to localhost:4000 then it will show hello world! on the browser. 
+})
+
+
+
+
+//03--- listen on port 4000:
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+})
