815cd275445cb6d5f68a53e440f2973ed135edd9
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-19 23:32:49 +0500

    code is added

--- NodeAPI/routes/user.js
+++ NodeAPI/routes/user.js
@@ -1,8 +1,55 @@
-// import express:
 import express from 'express';
 
 // create router:
 const router = express.Router();
+
+app.post('/users/new', async(req, res) => {
+    const {name, email, password} = req.body;
+    await user.create({
+        name,
+        email,
+        password,
+    })
+    res.status(201).cookie("temp", "lol").json({
+        success: true,
+        message: "User added successfully"});
+})
+
+app.get('/users/all', async(req, res) =>{
+    // -------Param in Postman --------------------
+    console.log(req.query);
+    console.log(req.query.noor);
+    //-----------------------------------------
+    const users = await user.find({});
+        res.json({
+            success: true,
+            users,
+        });
+});
+
+app.get("/userid/:id", async (req, res) => {
+    const { id } = req.params;
+    const userData = await user.findById(id);
+    // res.send(`The ID is: ${id}`);
+    res.json({
+        success: true,  
+        user: userData, 
+    })
+});
+
+app.get("/userid", async (req, res) => {
+    // const id = req.query.id;
+    // essy b destructure krskty hain:
+    const {id} = req.query;
+    const userData = await user.findById(id);
+    
+    res.json({
+        success: true,
+        // user: id,
+        user: userData, 
+        
+    })
+});
 
 
 // export router:
