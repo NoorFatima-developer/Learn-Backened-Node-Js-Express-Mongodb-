558f8f951c2bc633451bce40d7a29ab2f2373039
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-20 02:54:11 +0500

    code is added

--- NodeAPI/data/database.js
+++ NodeAPI/data/database.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
-// DB connection:
-mongoose.connect("mongodb://127.0.0.1:27017/", {
-    dbName: "backendapi",
-}).then(() => console.log("Database connected"))
-.catch((e) => console.log(e));
+// DB connection:(And wrap in function:)
 
-
+export const connectDB = () => {
+    mongoose.connect("mongodb://127.0.0.1:27017/", {
+        dbName: "backendapi",
+    }).then(() => console.log("Database connected"))
+    .catch((e) => console.log(e));
+}
