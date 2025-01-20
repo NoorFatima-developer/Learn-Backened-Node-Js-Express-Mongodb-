4f53acbfd8b410d5262104bbbad934634dd7009f
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-21 02:01:50 +0500

    code is added

--- TodoList/controllers/user.js
+++ TodoList/controllers/user.js
@@ -27,6 +27,9 @@ export const register = async(req, res) => {    // destruturing:
         password: hashedPassword,
     })
     // aghr tu m chahti o register successfuly hony k bd wo lpgin page pr redurect krjye tu i can use cookie for that:
+    // and cookies mai token secure form mai bejny klye we use jsonwebtoken...
+    // and cookie ka nam "token" hai and ab oss token ki value hum khud manual b deskty hain
+    // but hum chahty hain k oski value 
     // otherwise message hi thek hai...
     res.status(201).cookie("register", "successfully").json({
         success: true,
