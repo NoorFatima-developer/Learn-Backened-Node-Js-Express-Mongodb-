33776d34817fe934781946ad4079328cb3b43c49
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-21 01:08:18 +0500

    code is added

--- TodoList/routes/user.js
+++ TodoList/routes/user.js
@@ -3,6 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.post("/new", register);
+router.post("/login", login);
 router.get("/all", getAllUsers);
 router.get("/userid/:id", getUserid);
 
