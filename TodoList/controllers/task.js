9ba3b82e5c5639108164b7245134bf8bbb1b43ba
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-24 01:37:26 +0500

    code is added

--- TodoList/controllers/task.js
+++ TodoList/controllers/task.js
@@ -1,3 +1,17 @@
-export const newTask = (req, res) => {
-    
+
+import Task from "../models/task.js"
+export const newTask = async (req, res, next) => {
+
+    const {title, description} = req.body
+
+    // task create b 2 tareqo sy krskty hain:
+    // const task = new Task({title, description})
+    // await task.save();
+
+    // but hum create sy krty hain ku k wo 1 line mai hota hai:
+
+    await Task.create({
+        title, description, user:req.user,
+    })
+
 }
--- TodoList/routes/task.js
+++ TodoList/routes/task.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { newTask } from '../controllers/task';
+import { isAuthenticated } from '../middlewares/auth';
 
 const router = express.Router();
 
-router.post("/new", newTask)
+// iss sy pehly meny isAuthenticated islye use kea hai ku k m chahti o k jb user loggedin ho tb hi wo task add krsky.
+router.post("/new", isAuthenticated, newTask)
 
 export default router;
