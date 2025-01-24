be23d03d875dded0b911f6636431e81eb7ca4e1a
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-25 01:56:15 +0500

    code is added

--- TodoList/server.js
+++ TodoList/server.js
@@ -7,6 +7,6 @@ console.log(process.env.PORT);
 
 //listen on port 4000:
 app.listen(4000, () => {
-    console.log('Server is running on port 4000');
+    console.log(`Server is running on port: ${process.env.PORT}`);
 })
 
