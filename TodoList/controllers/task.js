86d77f6b211e5fab301a62f0e1905e1c8dbaf40e
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-24 20:30:08 +0500

    code is added

--- TodoList/controllers/task.js
+++ TodoList/controllers/task.js
@@ -6,32 +6,40 @@ import Task from "../models/task.js";
 // New Task..
 export const newTask = async (req, res, next) => {
 
-    const {title, description} = req.body
+//    use try catch...
 
-    // task create b 2 tareqo sy krskty hain:
-    // const task = new Task({title, description})
-    // await task.save();
+    try {
+        const {title, description} = req.body
 
-    // but hum create sy krty hain ku k wo 1 line mai hota hai:
+        // task create b 2 tareqo sy krskty hain:
+        // const task = new Task({title, description})
+        // await task.save();
+    
+        // but hum create sy krty hain ku k wo 1 line mai hota hai:
+    
+        await Task.create({
+            title,
+            description, 
+            // ye user wala islye dea hai ta k sirf whi user task add krsky jo login hai...
+            user:req.user,
+        })
+    
+        res.status(201).json({
+            success: true,
+            message: "Task created successfully",
+        })
 
-    await Task.create({
-        title,
-        description, 
-        // ye user wala islye dea hai ta k sirf whi user task add krsky jo login hai...
-        user:req.user,
-    })
-
-    res.status(201).json({
-        success: true,
-        message: "Task created successfully",
-    })
+    } catch (error) {
+        next(error)
+    }
 }
 
 
 // getAllTasks...
 export const getTasks = async (req, res, next) => {
 
-    const userid = req.user._id;
+    try {
+        const userid = req.user._id;
     // find method puri array return krta hai islye we will use find instead of findbyid...
     const task = await Task.find({user:userid})
 
@@ -42,32 +50,40 @@ export const getTasks = async (req, res, next) => {
         success: true,
         tasks:task,
     })
+    } catch (error) {
+        next(error)
+    }
 }
 
 // updateTask..
 export const updateTasks = async (req, res, next) => {
 
-    // const {id} = req.params;
-    const task = await Task.findById(req.params.id)
-    // ye line basically meny code chota krny klye ki hai and next app.js mai call hora hai..
-    // if(!task) return next(new Error())
-    // ab jesa k meny middleware mai errorhandler use krlea hai i will use that instead of error:
-
-    if(!task) return next(new ErrorHandler("Task not found", 404))
-
-    task.isCompleted = !task.isCompleted
-    await task.save();
-
-    res.status(200).json({
-        success: true,
-        message: "Task updated successfully",
-    })
+  try {
+      // const {id} = req.params;
+      const task = await Task.findById(req.params.id)
+      // ye line basically meny code chota krny klye ki hai and next app.js mai call hora hai..
+      // if(!task) return next(new Error())
+      // ab jesa k meny middleware mai errorhandler use krlea hai i will use that instead of error:
+  
+      if(!task) return next(new ErrorHandler("Task not found", 404))
+  
+      task.isCompleted = !task.isCompleted
+      await task.save();
+  
+      res.status(200).json({
+          success: true,
+          message: "Task updated successfully",
+      })
+  } catch (error) {
+    next(error);
+  }
 }
 
 // deleteTask...
 export const deleteTasks = async (req, res, next) => {
 
-    const task = await Task.findById(req.params.id)
+    try {
+        const task = await Task.findById(req.params.id)
 
         // amd aghr me new Error mai message ni deti tu mai wo msg middlware mai error file mai  b deskti o..
         // if(!task) return next(new Error("Invalid Id..."))
@@ -81,4 +97,7 @@ export const deleteTasks = async (req, res, next) => {
             success: true,
             message: "Task deleted successfully"
         })
+    } catch (error) {
+        next(error)
+    }
 }
