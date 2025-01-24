781ae430e0e5cd3a0695116a748f35d76ed42f25
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-24 17:57:51 +0500

    code is added

--- TodoList/controllers/task.js
+++ TodoList/controllers/task.js
@@ -60,7 +60,7 @@ export const deleteTasks = async (req, res, next) => {
     const task = await Task.findById(req.params.id)
 
         // amd aghr me new Error mai message ni deti tu mai wo msg middlware mai error file mai  b deskti o..
-        if(!task) return next(new Error(""))
+        if(!task) return next(new Error())
         await task.deleteOne();
 
         res.status(200).json({
--- TodoList/middlewares/error.js
+++ TodoList/middlewares/error.js
@@ -5,6 +5,6 @@ export const errorMiddleware = (err, req, res, next) => {
      return res.status(404)
     .json({
         success: false, 
-        message: err.message
+        message: err.message,
     })
 }
