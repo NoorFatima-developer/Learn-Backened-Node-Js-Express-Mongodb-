4a1b0346cc1c627e72e011afef47afb074a39a7a
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-24 05:01:15 +0500

    code is added

--- TodoList/routes/task.js
+++ TodoList/routes/task.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getTasks, newTask } from '../controllers/task.js';
+import { deleteTasks, getTasks, newTask, updateTasks } from '../controllers/task.js';
 import { isAuthenticated } from '../middlewares/auth.js';
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 // iss sy pehly meny isAuthenticated islye use kea hai ku k m chahti o k jb user loggedin ho tb hi wo task add krsky.
 router.post("/new", isAuthenticated, newTask)
 router.get("/me", isAuthenticated, getTasks)
-router.route("/:id").put().delete();
+router.route("/:id").put(updateTasks).delete(deleteTasks);
 
 export default router;
