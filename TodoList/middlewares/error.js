118c4be055513d2ef1e1b0c6566157333ea8e739
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-24 18:17:28 +0500

    code is added

--- TodoList/middlewares/error.js
+++ TodoList/middlewares/error.js
@@ -12,6 +12,8 @@ export const errorMiddleware = (err, req, res, next) => {
     console.log(err.message);
     
     err.message = err.message || "Internal Server Error";
+    err.statusCode = err.statusCode || 500;
+
      return res.status(404)
     .json({
         success: false, 
